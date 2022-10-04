ad2d6d8d4de495f9337532cc85c7ca91218269d8
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2022-10-04 23:26:22 +0530

    fixed the cors policy error by adding the proxy

--- server/index.js
+++ server/index.js
@@ -6,17 +6,17 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-// now we have to use the middleware to connect the routes to the applications. 
-// this route the urls to the proper routes in the router folder and then from there the handlers will be called 
-// this will make the server more scalable for this purpose 
-app.use('/posts', postRoutes);
-
-
 
 // setting the body parser so that we can properly send the request for this purpose to the application 
 app.use(bodyParser.json({limit : "30mb", extended : true}));
 app.use(bodyParser.urlencoded({limit : "30mb", extended : true}))
 app.use(cors());
+
+// now we have to use the middleware to connect the routes to the applications. 
+// this route the urls to the proper routes in the router folder and then from there the handlers will be called 
+// this will make the server more scalable for this purpose 
+app.use('/posts', postRoutes);
+
 
 // now we have to add the connection url for the hosted database over the cloud in the mongodb 
 const CONNECTION_URL = 'mongodb+srv://rupeshadmin:rupeshadmin123@cluster0.o49kxye.mongodb.net/?retryWrites=true&w=majority'
