982d10318e615ce071732442dbb88cdc65fbd9b6
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2022-10-04 16:34:02 +0530

    Added the style to the heading or the App component for this purpose

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,19 @@ import {Container , Typography, Grow, Grid, AppBar} from '@material-ui/core';
 import memories from "./images/memories.png";
 import {Posts} from "./components/Posts/Posts.js"
 import {Form} from "./components/Form/Form.js"
+import useStyles from "./styles.js"
 
 // defining the app functional component
 function App() {
+	const classes = useStyles();
   return (
 
     // defining the container that will automatically be aligned at the center for this purpose 
     <Container maxWidth = 'lg'>
-    	<AppBar position="static" color="inherit">
-    		<Typography variant = 'h2' align = "center"> Memories</Typography>
-        	<img src={memories} alt="memories" height="400"/>
-      	</AppBar>
+    	<AppBar className={classes.appBar} position="static" color="inherit">
+    		<Typography className = {classes.heading} variant = 'h2' align = "center"> Memories</Typography>
+        	<img className= {classes.image} src={memories} alt="memories" height="60"/>
+      	</AppBar> 
       	<Grow in>
         	<Container>
           		<Grid container justify = "space-between" alignItems="stretch" spacing={4}>
