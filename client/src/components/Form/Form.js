dfd7cd99681eea5501ccb16b83ab541dbddd215a
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2022-10-04 16:42:22 +0530

    added the styled or coped the style to the form and the posts components. Now moving ahead to implement the api to be able to communicate between the server and the client

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,8 +1,10 @@
 // this is form for the frontend for this purpose 
 import React from "react";
+import useStyles from "./styles.js"
 
 // defining the form component 
 export const Form = ()=>{
+    const classes = useStyles();
     return (
         <>
             <h2>Form</h2>
---
+++ client/src/components/Posts/styles.js
@@ -0,0 +1,16 @@
+// adding the style for the posts component 
+
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  mainContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  smMargin: {
+    margin: theme.spacing(1),
+  },
+  actionDiv: {
+    textAlign: 'center',
+  },
+}));
