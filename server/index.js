570e96903affb8ed40ad34f6a12d3ce7894ef03a
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2022-10-04 09:31:25 +0530

    set up the server side code for this memories application

---
+++ server/index.js
@@ -0,0 +1,13 @@
+// const express = require("express");
+// import { Express } from "express";
+import pkg from 'express';
+const { Express } = pkg;
+
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+
+console.log("hi from the developer side for this purpose \n");
+
+
