0e1f433f46e5891c1672e909efbd035805722535
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2022-10-04 11:05:12 +0530

    added the simple controller for the getPosts router. Now adding the other routes and the controllers for this purpose

---
+++ server/controllers/posts.js
@@ -0,0 +1,6 @@
+// in this all the handlers related to the posts will be done or the business logic will be written in this file itself 
+
+// this will help to make the application more scalable. 
+export const getPosts = (req, res)=>{
+    res.send("This works");
+}
