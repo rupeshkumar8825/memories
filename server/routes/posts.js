a4c5cd16dc0197daf150fc73fbbaa6c9f0f257c7
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2022-11-23 02:05:14 +0530

    Added some of the comments to know about the actual better way of routing the apis on the server side

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -8,6 +8,7 @@ const router = express.Router();
 
 // we will not write the business logic in this because the business logic will be large and hence this file will also become large due to which managing this particular file will be very hard and will be totally lost while searching the routes and adding the new routes 
 
+// hence this file will be center point for all the apis related to the posts  and i will be routing these apis endpoints to the actual end points as shown below  
 router.get('/', getPosts);
 router.post('/', createPost);
 
